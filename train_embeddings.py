6bc8ac5bd01883059172c078483b06c34b84a0ff
Diego Lopez Yse <40613836+dlopezyse@users.noreply.github.com>
2023-11-09 21:06:31 -0300

    Create train_embeddings.py

---
+++ train_embeddings.py
@@ -0,0 +1,31 @@
+# Start by training model on give dataset
+########################################################
+
+# Import dataset
+
+from pykeen.datasets import DRKG
+
+dataset = DRKG()
+dataset.summarize()
+
+
+# Train embedding model on dataset
+########################################################
+
+from pykeen.pipeline import pipeline
+
+pipeline_result = pipeline(
+    dataset="DRKG",
+    model="TransE",
+    # Training configuration
+    training_kwargs=dict(
+        num_epochs=50,
+        use_tqdm_batch=False,
+    ),
+    # Runtime configuration
+    random_seed=1234,
+    device="gpu",
+)
+
+# Save training triples to directory
+pipeline_result.save_to_directory('./DRKG/DRKG_transe')
